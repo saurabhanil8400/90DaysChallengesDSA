288e788357c6690e4172c5d36445be8524fb6cce
Saurabh Singh <93283916+saurabhanil8400@users.noreply.github.com>
2023-06-26 00:14:36 +0530

    find unique  element from the array

---
+++ uniqueE.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class uniqueE{
+     static void  findUnique(int arr[],int n){
+       int ans=0;
+       for(int i=0;i<arr.length;i++){
+        ans=ans^arr[i];
+        
+       }System.out.print(ans);
+       
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("OUTPUT");
+        findUnique(arr,n);
+        
+
+
+        
+        
+
+
+    }
+    
+}
